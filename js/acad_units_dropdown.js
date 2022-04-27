864d2b979011f893a3e31ee5fb37ca5f7f4522f2
Sean Gabriel Bayron <scbayron@up.edu.ph>
2022-04-28 00:58:16 +0800

    more updates. jquery application

--- js/acad_units_dropdown.js
+++ js/acad_units_dropdown.js
@@ -8,3 +8,69 @@ $(document).ready(function() {
         }
     );
 });
+
+// Function for showing/hiding forms
+// function next(a) {
+//     // if next is inserted with a specific integer, it will hide a specific form and show another one
+//     if (a == 1) {
+//         document.getElementById("hss").style.display = "block";
+//         document.getElementById("csm").style.display = "none";
+//         document.getElementById("som").style.display = "none";
+//     }
+//     if (a == 2) {
+//         document.getElementById("csm").style.display = "block";
+//         document.getElementById("hss").style.display = "none";
+//         document.getElementById("som").style.display = "none";
+//     }
+//     if (a == 3) {
+//         document.getElementById("som").style.display = "block";
+//         document.getElementById("hss").style.display = "none";
+//         document.getElementById("csm").style.display = "none";
+//     }
+// }
+
+
+// function open_page() {
+//     var path = window.location.pathname;
+//     var page = path.split("/").pop();
+
+//     if (page !== "acad_units.html") {
+//         window.location.href = "acad_units.html";
+//     }
+// }
+
+var path = window.location.pathname;
+var page = path.split("/").pop();
+console.log(page);
+
+$(document).ready(function() {
+    $("[value='hss']").click(function() {
+        if (page !== "acad_units.html") {
+            window.location.href = "acad_units.html";
+            $(document).ready(function() {
+                $(".hss").css('display', 'block');
+            })
+        }
+        $(".hss").css('display', 'block');
+        $(".csm").css('display', 'none');
+        $(".som").css('display', 'none');
+    });
+    $("[value='csm']").click(function() {
+        if (page !== "acad_units.html") {
+            window.location.href = "acad_units.html";
+            $(".csm").css('display', 'block');
+        }
+        $(".hss").css('display', 'none');
+        $(".csm").css('display', 'block');
+        $(".som").css('display', 'none');
+    });
+    $("[value='som']").click(function() {
+        if (page !== "acad_units.html") {
+            window.location.href = "acad_units.html";
+            $(".som").css('display', 'block');
+        }
+        $(".hss").css('display', 'none');
+        $(".csm").css('display', 'none');
+        $(".som").css('display', 'block');
+    });
+})
