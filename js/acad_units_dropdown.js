c752aa06e154e27004c42058fb1fe5abe62f07d6
Sean Gabriel Bayron <scbayron@up.edu.ph>
2022-04-29 18:06:04 +0800

    some more changes

--- js/acad_units_dropdown.js
+++ js/acad_units_dropdown.js
@@ -9,36 +9,6 @@ $(document).ready(function() {
     );
 });
 
-// Function for showing/hiding forms
-// function next(a) {
-//     // if next is inserted with a specific integer, it will hide a specific form and show another one
-//     if (a == 1) {
-//         document.getElementById("hss").style.display = "block";
-//         document.getElementById("csm").style.display = "none";
-//         document.getElementById("som").style.display = "none";
-//     }
-//     if (a == 2) {
-//         document.getElementById("csm").style.display = "block";
-//         document.getElementById("hss").style.display = "none";
-//         document.getElementById("som").style.display = "none";
-//     }
-//     if (a == 3) {
-//         document.getElementById("som").style.display = "block";
-//         document.getElementById("hss").style.display = "none";
-//         document.getElementById("csm").style.display = "none";
-//     }
-// }
-
-
-// function open_page() {
-//     var path = window.location.pathname;
-//     var page = path.split("/").pop();
-
-//     if (page !== "acad_units.html") {
-//         window.location.href = "acad_units.html";
-//     }
-// }
-
 var path = window.location.pathname;
 var page = path.split("/").pop();
 console.log(page);
