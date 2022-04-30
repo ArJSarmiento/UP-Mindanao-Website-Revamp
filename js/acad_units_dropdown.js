5d21165426665dda824d301a1d78678d6b773aa1
Sean Gabriel Bayron <scbayron@up.edu.ph>
2022-04-30 22:51:51 +0800

    removed unnecessary line of codes from css and js

--- js/acad_units_dropdown.js
+++ js/acad_units_dropdown.js
@@ -10,42 +10,4 @@ $(document).ready(function() {
         var option = $(this).find('option:selected');
         window.location.href = option.data("url");
     });
-
-
-    // var path = window.location.pathname;
-    // var page = path.split("/").pop();
-    // console.log(page);
-
-    // $("[data-url='hss']").click(function() {
-    //     if (page !== "acad_units.html") {
-    //         window.location.href = "acad_units.html";
-    //         $(document).ready(function() {
-    //             $(".hss").css('display', 'block');
-    //         })
-    //     }
-    //     $(".hss").css('display', 'block');
-    //     $(".csm").css('display', 'none');
-    //     $(".som").css('display', 'none');
-    //     $(".gradient-background").css('background-image', 'linear-gradient(#0A3A3A, #fff)')
-    // });
-    // $("[data-url='csm']").click(function() {
-    //     if (page !== "acad_units.html") {
-    //         window.location.href = "acad_units.html";
-    //         $(".csm").css('display', 'block');
-    //     }
-    //     $(".hss").css('display', 'none');
-    //     $(".csm").css('display', 'block');
-    //     $(".som").css('display', 'none');
-    //     $(".gradient-background").css('background-image', 'linear-gradient(#7C0000, #fff)')
-    // });
-    // $("[data-url='som']").click(function() {
-    //     if (page !== "acad_units.html") {
-    //         window.location.href = "acad_units.html";
-    //         $(".som").css('display', 'block');
-    //     }
-    //     $(".hss").css('display', 'none');
-    //     $(".csm").css('display', 'none');
-    //     $(".som").css('display', 'block');
-    //     $(".gradient-background").css('background-image', 'linear-gradient(#F3BD25, #fff)')
-    // });
 })
