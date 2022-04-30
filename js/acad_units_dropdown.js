628e0e9a39decf33f5160bc6365d78a564fa1f68
Sean Gabriel Bayron <scbayron@up.edu.ph>
2022-04-30 22:40:55 +0800

    Reimplementation of Dropdown
    -Changed Dropdown reimplementation
    -Added individual html files for each academic unit

--- js/acad_units_dropdown.js
+++ js/acad_units_dropdown.js
@@ -1,49 +1,51 @@
-$(document).ready(function () {
-    $(".drop-down").hover(
-        function () {
-            $('>ul.sub:not(:animated)', this).slideDown(500);
-        },
-        function () {
-            $('>ul.sub', this).slideUp(300);
-        }
-    );
+$(document).ready(function() {
 
+    $("#dropdown").selectmenu();
 
-    var path = window.location.pathname;
-    var page = path.split("/").pop();
-    console.log(page);
-
-
-    $("[value='hss']").click(function () {
-        if (page !== "acad_units.html") {
-            window.location.href = "acad_units.html";
-            $(document).ready(function () {
-                $(".hss").css('display', 'block');
-            })
-        }
-        $(".hss").css('display', 'block');
-        $(".csm").css('display', 'none');
-        $(".som").css('display', 'none');
-        $(".gradient-background").css('background-image', 'linear-gradient(#0A3A3A, #fff)')
+    $('.label-acad-u').click(function() {
+        $('.sub1').toggle()
     });
-    $("[value='csm']").click(function () {
-        if (page !== "acad_units.html") {
-            window.location.href = "acad_units.html";
-            $(".csm").css('display', 'block');
-        }
-        $(".hss").css('display', 'none');
-        $(".csm").css('display', 'block');
-        $(".som").css('display', 'none');
-        $(".gradient-background").css('background-image', 'linear-gradient(#7C0000, #fff)')
+
+    $('#dropdown').on('selectmenuchange', function() {
+        var option = $(this).find('option:selected');
+        window.location.href = option.data("url");
     });
-    $("[value='som']").click(function () {
-        if (page !== "acad_units.html") {
-            window.location.href = "acad_units.html";
-            $(".som").css('display', 'block');
-        }
-        $(".hss").css('display', 'none');
-        $(".csm").css('display', 'none');
-        $(".som").css('display', 'block');
-        $(".gradient-background").css('background-image', 'linear-gradient(#F3BD25, #fff)')
-    });
+
+
+    // var path = window.location.pathname;
+    // var page = path.split("/").pop();
+    // console.log(page);
+
+    // $("[data-url='hss']").click(function() {
+    //     if (page !== "acad_units.html") {
+    //         window.location.href = "acad_units.html";
+    //         $(document).ready(function() {
+    //             $(".hss").css('display', 'block');
+    //         })
+    //     }
+    //     $(".hss").css('display', 'block');
+    //     $(".csm").css('display', 'none');
+    //     $(".som").css('display', 'none');
+    //     $(".gradient-background").css('background-image', 'linear-gradient(#0A3A3A, #fff)')
+    // });
+    // $("[data-url='csm']").click(function() {
+    //     if (page !== "acad_units.html") {
+    //         window.location.href = "acad_units.html";
+    //         $(".csm").css('display', 'block');
+    //     }
+    //     $(".hss").css('display', 'none');
+    //     $(".csm").css('display', 'block');
+    //     $(".som").css('display', 'none');
+    //     $(".gradient-background").css('background-image', 'linear-gradient(#7C0000, #fff)')
+    // });
+    // $("[data-url='som']").click(function() {
+    //     if (page !== "acad_units.html") {
+    //         window.location.href = "acad_units.html";
+    //         $(".som").css('display', 'block');
+    //     }
+    //     $(".hss").css('display', 'none');
+    //     $(".csm").css('display', 'none');
+    //     $(".som").css('display', 'block');
+    //     $(".gradient-background").css('background-image', 'linear-gradient(#F3BD25, #fff)')
+    // });
 })
