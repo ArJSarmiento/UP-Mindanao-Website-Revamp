3a1c3d4d9c19161e2c3ea010613985da383680c6
Arnel Jan Sarmiento <aesarmiento@up.edu.ph>
2022-05-01 09:43:54 +0800

    Added comments for js files

--- js/acad_units_dropdown.js
+++ js/acad_units_dropdown.js
@@ -1,13 +1,16 @@
-$(document).ready(function() {
-
+//waits for html to load before running the script
+$(document).ready(function () {
+    //select menu initialization
     $("#dropdown").selectmenu();
 
-    $('.label-acad-u').click(function() {
+    //togles select menu is nav button is clicked
+    $('.label-acad-u').click(function () {
         $('.sub1').toggle()
     });
 
-    $('#dropdown').on('selectmenuchange', function() {
-        var option = $(this).find('option:selected');
+    //redirects to the url of the select option
+    $('#dropdown').on('selectmenuchange', function () {
+        let option = $(this).find('option:selected');
         window.location.href = option.data("url");
     });
 })
--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-$(document).ready(function() {
-    $(function() {
+//waits for html to load before running the script
+$(document).ready(function () {
+    //tabs initialization
+    $(function () {
         $("#tabs").tabs();
     });
 })
