1d77b6a01956f7966bb85153d20017f96b66a103
Arnel Jan Sarmiento <aesarmiento@up.edu.ph>
2022-05-02 13:36:51 +0800

    Grammar check and comments

--- js/academic-programs.js
+++ js/academic-programs.js
@@ -1,9 +1,10 @@
+//waits for html to load before running the script
 $(document).ready(function () {
   $(function () {
+    //initialization of jquery accrodion
     $("#accordion").accordion({
       collapsible: true,
       heightStyle: "content"
     });
   });
 })
-
