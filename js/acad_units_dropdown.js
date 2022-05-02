7690ad8145c22fdb486dd305c3a43747315b68f8
Sean Gabriel Bayron <scbayron@up.edu.ph>
2022-05-02 13:41:49 +0800

    Comments for Academic Units HTMLs, CSS, and JS

--- js/acad_units_dropdown.js
+++ js/acad_units_dropdown.js
@@ -1,15 +1,16 @@
 //waits for html to load before running the script
-$(document).ready(function () {
+$(document).ready(function() {
     //select menu initialization
     $("#dropdown").selectmenu();
 
-    //togles select menu is nav button is clicked
-    $('.label-acad-u').click(function () {
+    //toglges select menu is nav button is clicked
+    $('.label-acad-u').click(function() {
         $('.sub1').toggle()
     });
 
+    // selectmenu() function
     //redirects to the url of the select option
-    $('#dropdown').on('selectmenuchange', function () {
+    $('#dropdown').on('selectmenuchange', function() {
         let option = $(this).find('option:selected');
         window.location.href = option.data("url");
     });
